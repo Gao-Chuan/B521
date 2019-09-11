e248a6aa328d27282f46e969108e258373fa1bba
gaochuan <chuan@localhost.localdomain>
2019-09-11 11:30:18 -0400

    a2

--- A2/a2.rkt
+++ A2/a2.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-;; Part 1
+;; Part 1. Natural Recursion Refresher
 ;; Case 1. list-ref
 (define list-ref
   (lambda (ls n)
@@ -57,4 +57,165 @@
 ;;; (walk-symbol 'd '((a . 5) (b . (1 2)) (c . a) (e . c) (d . e)))
 ;;; (walk-symbol 'd '((a . 5) (b . 6) (c . f) (e . c) (d . e)))
 
-;; Part 2.
+;; Part 2. Free, Bound, Lexical Address
+;; Case 5. lambda->lumbda
+(define lambda->lumbda
+  (lambda (exp)
+    (match exp
+      [`,y
+        #:when (symbol? y) y]
+      [`(lambda (,x) ,body)
+        `(lumbda (,x) ,(lambda->lumbda body))]
+      [`(,rator ,rand)
+        (list (lambda->lumbda rator) (lambda->lumbda rand))])))
+;;; (lambda->lumbda 'x)
+;;; (lambda->lumbda '(lambda (x) x))
+;;; (lambda->lumbda '(lambda (z) ((lambda (y) (a z)) (h (lambda (x) (h a))))))
+;;; (lambda->lumbda '(lambda (lambda) lambda))
+;;; (lambda->lumbda '((lambda (lambda) lambda) (lambda (y) y)))
+;;; (lambda->lumbda '((lambda (x) x) (lambda (x) x)))
+
+;;; Case 6. var-occurs?
+(define var-occurs?
+  (lambda (var exp)
+    (match exp
+      [`,y
+        #:when (symbol? y)
+        (eqv? y var)]
+      [`(lambda (,x) ,body)
+        (var-occurs? var body)]
+      [`(,rator ,rand)
+        (or (var-occurs? var rator) (var-occurs? var rand))])))
+;;; (var-occurs? 'x 'x) 
+;;; (var-occurs? 'x '(lambda (x) y))
+;;; (var-occurs? 'x '(lambda (y) x))
+;;; (var-occurs? 'x '((z y) x))
+
+;;; Case 7. vars
+(define vars
+  (lambda (exp)
+    (match exp
+      [`,y
+        #:when (symbol? y) `(,y)]
+      [`(lambda (,x) ,body)
+        (vars body)]
+      [`(,rator ,rand)
+        (append (vars rator) (vars rand))])))
+;;; (vars 'x)
+;;; (vars '(lambda (x) x))
+;;; (vars '((lambda (y) (x x)) (x y)))
+;;; (vars '(lambda (z) ((lambda (y) (a z)) (h (lambda (x) (h a))))))
+
+;;; Case 8. unique-vars
+(define unique-vars
+  (lambda (exp)
+    (match exp
+      [`,y
+        #:when (symbol? y) `(,y)]
+      [`(lambda (,x) ,body)
+        (unique-vars body)]
+      [`(,rator ,rand)
+        (union (unique-vars rator) (unique-vars rand))])))
+;;; (unique-vars '((lambda (y) (x x)) (x y)))
+;;; (unique-vars '((lambda (z) (lambda (y) (z y))) x))
+;;; (unique-vars '((lambda (a) (a b)) ((lambda (c) (a c)) (b a))))
+
+;;; Case 9. var-occurs-free?
+(define var-occurs-free?
+  (lambda (var exp)
+    (match exp
+      [`,y
+        #:when (symbol? y)
+        (eqv? y var)]
+      [`(lambda (,x) ,body)
+        (if (not (eqv? x var))
+        (var-occurs-free? var body)
+        #f)]
+      [`(,rator ,rand)
+        (or (var-occurs-free? var rator) (var-occurs-free? var rand))])))
+;;; (var-occurs-free? 'z  '((lambda (z) (lambda (y) (z y))) x))
+;;; (var-occurs-free? 'x 'x)
+;;; (var-occurs-free? 'x '(lambda (y) y))
+;;; (var-occurs-free? 'x '(lambda (x) (x y)))
+;;; (var-occurs-free? 'x '(lambda (x) (lambda (x) x))) 
+;;; (var-occurs-free? 'y '(lambda (x) (x y)))
+;;; (var-occurs-free? 'y '((lambda (y) (x y)) (lambda (x) (x y))))
+;;; (var-occurs-free? 'x '((lambda (x) (x x)) (x x)))
+;;; (var-occurs-free? 'x '(lambda (x) (lambda (y) (x)))) ;;; not free?
+
+;;; Case 10. var-occurs-bound?
+(define var-occurs-bound?
+  (lambda (var exp)
+    (match exp
+      [`,y
+        #:when (symbol? y)
+        #f]
+      [`(lambda (,x) ,body)
+        (if (not (eqv? x var))
+        (var-occurs-bound? var body)
+        (or (var-occurs-bound? var body) (var-occurs-free? var body)))]
+      [`(,rator ,rand)
+        (or (var-occurs-bound? var rator) (var-occurs-bound? var rand))])))
+;;; (var-occurs-bound? 'x 'x)
+;;; (var-occurs-bound? 'x '(lambda (x) x))
+;;; (var-occurs-bound? 'y '(lambda (x) x))
+;;; (var-occurs-bound? 'x '((lambda (x) (x x)) (x x)))
+;;; (var-occurs-bound? 'z '(lambda (y) (lambda (x) (y z))))
+;;; (var-occurs-bound? 'z '(lambda (y) (lambda (z) (y z))))
+;;; (var-occurs-bound? 'x '(lambda (x) y))
+;;; (var-occurs-bound? 'x '(lambda (x) (lambda (x) x)))
+
+;;; Case 11. unique-free-vars
+(define unique-free-vars
+  (lambda (exp)
+    (match exp
+      [`,y
+        #:when (symbol? y) `(,y)]
+      [`(lambda (,x) ,body)
+        (remv x (unique-free-vars body))]
+      [`(,rator ,rand)
+        (union (unique-free-vars rator) (unique-free-vars rand))])))
+;;; (unique-free-vars 'x)
+;;; (unique-free-vars '(lambda (x) (x y)))
+;;; (unique-free-vars '((lambda (x) ((x y) e)) (lambda (c) (x (lambda (x) (x (e c)))))))
+
+;;; Case 12. unique-bound-vars
+(define unique-bound-vars
+  (lambda (exp)
+    (match exp
+      [`,y
+        #:when (symbol? y)
+        '()]
+      [`(lambda (,x) ,body)
+        (if (var-occurs-free? x body)
+          (union `(,x) (unique-bound-vars body))
+          (unique-bound-vars body))]
+      [`(,rator ,rand)
+        (union (unique-bound-vars rator) (unique-bound-vars rand))])))
+;;; (unique-bound-vars 'x)
+;;; (unique-bound-vars '(lambda (x) y))
+;;; (unique-bound-vars '(lambda (x) (x y)))
+;;; (unique-bound-vars '((lambda (x) ((x y) e)) (lambda (c) (x (lambda (x) (x (e c)))))))
+;;; (unique-bound-vars '(lambda (y) y))
+;;; (unique-bound-vars '(lambda (x) (y z)))
+;;; (unique-bound-vars '(lambda (x) (lambda (x) x)))
+
+;;; Case 13. lex
+(define lex
+  (lambda (exp acc)
+    (match exp
+      [`,y
+        #:when (symbol? y)
+        `(var ,(cdr (assv y acc)))]
+      [`(lambda (,x) ,body)
+        #:when (and (symbol? x) (null? acc))
+        (list 'lambda (lex body `((,x . 0))))]
+      [`(lambda (,x) ,body)
+        #:when (symbol? x)
+         (if (assv x acc)
+         (list 'lambda (lex body (cons `(,x . 0) (remv (assv x acc) (map (lambda (p) ((car p) . (+ 1 (cdr p)))) acc)))))
+         (list 'lambda (lex body (cons `(,x . 0) (map (lambda (p) ((car p) . (+ 1 (cdr p)))) acc)))))]
+      [`(,rator ,rand)
+        (list (lex rator acc) (lex rand acc))])))
+(lex '(lambda (x) x) '())
+(lex '(lambda (y) (lambda (x) y)) '())
--- test.rkt
+++ test.rkt
@@ -1,16 +1,69 @@
 #lang racket
 
-(define + (lambda (n m) (
-    cond
-    [(zero? m) n]
-    [else (add 1 (+ n (sub1 m)))]
-)))
+;;; (define + (lambda (n m) (
+;;;     cond
+;;;     [(zero? m) n]
+;;;     [else (add 1 (+ n (sub1 m)))]
+;;; )))
 
-(define *
-    (lambda (n m)
-        (cond
-            [(zero? m) 0]
-            [else (* n (sub1 m))]
-        )    
-    )
-)
+;;; (define *
+;;;     (lambda (n m)
+;;;         (cond
+;;;             [(zero? m) 0]
+;;;             [else (* n (sub1 m))]
+;;;         )    
+;;;     )
+;;; )
+
+;;; Environment
+(define table
+    (lambda (var)
+        (match var
+            [`a 'ant]
+            [`b 'boar]
+            [`c 'cat]
+            [`d 'elephant])))
+
+;;; (define apply-env
+;;;     (lambda (env var)
+;;;         (env var)))
+
+;;; (define ext-env
+;;;   (lambda (env var val)
+;;;     (lambda (y)
+;;;       (if (eqv? y var) val (env y)))))
+
+;;;  (define table2
+;;;     (ext-env table 'e 'dog))
+
+;;; (apply-env table2 'e)
+;;; (apply-env table2 'a)
+
+(define init-env
+    (lambda ()
+        '()))
+
+(define ext-env
+    (lambda (env x val)
+        `((,x . , val) . ,env)))
+
+(define apply-env
+    (lambda (env var)
+        (match env
+            ['() (error "whatever")]
+            [`((,x . , val) . ,env)
+                (if (eqv? x var)
+                    val
+                    (apply-env env var))])))
+
+(define table1 
+    (ext-env (init-env) 'd 'elephent))
+
+(define table2
+    (ext-env table1 'a 'test))
+
+
+(apply-env table2 'a)
+(apply-env table2 'd)
+
+;;; to improve is to change, so to be perfect is to have changed often? is it true?
