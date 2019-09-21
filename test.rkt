2ff8edc2f4d5fc707b27d5d4479009c4e5e0e815
Gao Chuan <g40.chuan@gmail.com>
2019-09-21 18:51:44 -0400

    a3

--- test.rkt
+++ test.rkt
@@ -5,3 +5,15 @@
     [(t u) (values 3 4)]
 )   (+ x u)
 )
+
+(define valof
+    (lambda (e env)
+        (match e
+            [`,y 
+                #:when (symbol? y) 
+                (env x)]
+            [`(lambda (,x) ,body) 
+                #:when (symbol? x) 
+                (lambda (arg) (valof body (lambda y (if (eqv? y x) arg (env y)))))]
+            [`(,rator ,rand)
+                ((valof rand env) (valof rand env))])))
