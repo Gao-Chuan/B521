abcb8d853eb7d9850c733531b80a37c6a1d7baaf
1phan <g40.chuan@gmail.com>
2019-10-30 22:50:24 -0400

    a8

--- A8/a8.rkt
+++ A8/a8.rkt
@@ -22,6 +22,18 @@
             [`(fact-ck ,n ,k) (begin (set! rv (* n rv))
                                      (set! rk k)
                                      (apply-rk))]
+            [`(pascal-init ,k) (begin (set! rm 1)
+                                      (set! ra 0)
+                                      (set! rk k)
+                                      (rv))]
+            [`(pascal-ck ,m ,k) (begin (set! rm (add1 rm))
+                                          (set! rk k)
+                                          (set! rk (pascal-ck-in))
+                                          (rv))]
+            [`(pascal-ck-in ,k ,a) (begin (set! ra a)
+                                        (set! rv (cons ra rv))
+                                        (set! rk k)
+                                        (apply-rk))]
         )
     )
 )
@@ -145,4 +157,102 @@
 ; (fact-reg-driver 5)
 
 ; pascal
-(define )
+(define rpascal #f)
+(define ra #f)
+
+(define pascal-ck-in
+    (lambda()
+        `(pascal-ck-in ,rk ,ra)
+    )
+)
+
+(define pascal-ck
+    (lambda()
+        `(pascal-ck ,rm ,rk)
+    )
+)
+
+(define pascal-init
+    (lambda()
+        `(pascal-init ,rk)
+    )
+)
+
+(define pascal-reg
+    (lambda ()
+        (begin 
+            (set! rpascal (lambda ()
+                            (begin
+                                (set! rv (lambda ()
+                                            (cond
+                                                [(> rm rn) (begin
+                                                                (set! rv '())
+                                                                (apply-rk))]
+                                                [else (begin
+                                                        (set! ra (+ ra rm))
+                                                        (set! rk (pascal-ck))
+                                                        (rpascal)
+                                                      )]
+                                            )
+                                        ))
+                                (apply-rk)
+                            )
+                          ))
+            (set! rk (pascal-init))
+            (rpascal)
+        )
+    )
+)
+
+(define pascal-reg-driver
+    (lambda (n)
+        (begin (set! rn n)
+                (set! rk (empty-k))
+                (pascal-reg))
+    )
+)
+; (pascal-reg-driver 10)
+
+; Brainteaser
+(define fib-cps
+  (lambda (n k)
+  (lambda()
+    (cond
+      [(and (not (negative? n)) (< n 2)) (k n)]
+      [else (fib-cps (sub1 n) (lambda (v) 
+                                (fib-cps (sub1 (sub1 n)) (lambda (t)
+                                                            (k (+ v t))))))]))))
+
+(define ramp-empty-k
+    (lambda (k) k)
+)
+
+(define rampoline
+    (lambda(t1 t2 t3)
+        (let ([r (random 3)])
+            (match r
+                [0 (rampoline (t1) t2 t3)]
+                [1 (rampoline t1 (t2) t3)]
+                [2 (rampoline t1 t2 (t3))]
+            )
+        )
+    )
+)
+
+(define fib-ramp-driver
+  (lambda (n1 n2 n3)
+    (let/cc jumpout
+      (rampoline
+        (lambda ()
+          (fib-cps n1 (ramp-empty-k jumpout)))
+        (lambda ()
+          (fib-cps n2 (ramp-empty-k jumpout)))
+        (lambda ()
+          (fib-cps n3 (ramp-empty-k jumpout)))))))
+; (require racket/trace)
+; (trace fib-cps)
+
+; (fib-ramp-driver 6 -1 -1)
+
+; Just Dessert
+
